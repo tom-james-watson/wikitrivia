7a8005446dec6f67b7a179ea059c64f5cd4cfeb5
Trevor Blanarik <tblanarik@github.com>
2022-01-25 01:17:44 +0000

    added a few more clearly wrong ones

--- lib/bad-cards.ts
+++ lib/bad-cards.ts
@@ -1,6 +1,15 @@
 const badCards = {
     'Q745019': 'Colt\'s Manufacturing Company',
     'Q697675': 'Gigabyte Technology',
+    'Q157064': 'Puma (brand)',
+    'Q25716': '1st millenium',
+    'Q134862': 'Champagne',
+    'Q179900': 'David (Michelangelo)',
+    'Q218567': 'Law & Order SVU',
+    'Q486682': 'Crips',
+    'Q12871': 'Simon the Zealot',
+    'Q5505': 'Lake Victoria',
+    'Q58784': 'Das Kapital',
 }
 
 export default badCards
