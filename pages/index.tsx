91ed0eeb274699146b85a792fd43070b6bc82573
Tom Watson <tom@tomjwatson.com>
2022-01-30 11:54:46 +0100

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
   return (
     <>
       <Head>
-        <title>Wiki History Game</title>
+        <title>Wikitrivia</title>
         <link
           rel="shortcut icon"
           href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%8F%9B%EF%B8%8F%3C%2Ftext%3E%3C%2Fsvg%3E"
