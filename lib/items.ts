d47a0d9155bb4ede27e12a3ebba008ebdd16a824
jbates <johnalexanderbates@gmail.com>
2022-02-13 20:46:54 -0800

    increasing difficulty: keep a minimum distance of 3 years between cards, with a larger min dist for very long-ago events

--- lib/items.ts
+++ lib/items.ts
@@ -7,15 +7,21 @@ export function getRandomItem(deck: Item[], played: Item[]): Item {
   });
 
   const periods: [number, number][] = [
-    [-100000, 1400],
-    [1400, 1850],
-    [1850, 1930],
     [1930, 2020],
+    [1850, 1930],
+    [1400, 1850],
+    [-100000, 1400],
   ];
+
+  const thisperiod = Math.floor(Math.random() * periods.length);  
   const [fromYear, toYear] =
-    periods[Math.floor(Math.random() * periods.length)];
+    periods[thisperiod];
   const avoidPeople = Math.random() > 0.5;
 
+  // as suggested by @benguraldi  
+  let distance = 30 - 3 * played.length;
+  distance = distance < 3 ? 3 : distance;
+    
   const candidates = deck.filter((candidate) => {
     if (avoidPeople && candidate.instance_of.includes("human")) {
       return false;
@@ -25,12 +31,34 @@ export function getRandomItem(deck: Item[], played: Item[]): Item {
       return false;
     }
 
-    if (playedYears.includes(candidate.year)) {
-      return false;
+    // tweak on @benguraldi suggestion
+    for (let i = 0; i < played.length; i++) {
+	if (Math.abs(candidate.year - played[i].year) < distance * (thisperiod + 1)) {
+	    return false;
+	}
     }
 
     return true;
   });
+
+
+  if (candidates.length === 0) {
+    const candidates = deck.filter((candidate) => {
+      if (avoidPeople && candidate.instance_of.includes("human")) {
+        return false;
+      }
+  
+      if (candidate.year < fromYear || candidate.year > toYear) {
+        return false;
+      }
+  
+      if (playedYears.includes(candidate.year)) {
+        return false;
+      }
+  
+      return true;
+    });
+  }
 
   if (candidates.length === 0) {
     throw new Error("No item candidates");
