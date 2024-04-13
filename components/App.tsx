ef0fd708efd413b643312cd7b43025b1764e8057
flaburgan <flaburgan@geexxx.fr>
2024-04-14 00:11:23 +0200

    Dynamically load the locales, so only one is loaded

--- components/App.tsx
+++ components/App.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
-import { messages as enMessages } from "../locales/en/messages";
-import { messages as frMessages } from "../locales/fr/messages";
 import { t } from "@lingui/macro";
 import Game from "./game";
 
+export const locales = ["en", "fr"];
+export const defaultLocale = "en";
+
+/**
+ * We do a dynamic import of just the catalog that we need
+ * @param locale any locale string
+ */
+export async function dynamicActivate(locale: string) {
+  const { messages } = await import(`../locales/${locale}/messages`);
+  i18n.load(locale, messages);
+  i18n.activate(locale);
+}
+
 export default function App() {
-  i18n.load({
-    en: enMessages,
-    fr: frMessages,
-  });
-  i18n.activate(navigator.language);
+  useEffect(() => {
+    const lang = navigator.language.substring(0,2);
+    // With this method we dynamically load the catalogs
+    dynamicActivate(locales.includes(lang) ? lang : defaultLocale);
+  }, []);
 
   return <I18nProvider i18n={i18n}>
     <Head>
