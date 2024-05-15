beb6e9d10cd449963d979ea877b1097b1a656e9d
flaburgan <flaburgan@geexxx.fr>
2024-05-15 21:53:51 +0200

    Render the dashed timeline with CSS only

--- components/played-item-list.tsx
+++ components/played-item-list.tsx
@@ -22,9 +22,6 @@ export default function PlayedItemList(props: PlayedItemListProps) {
               {...provided.droppableProps}
               className={styles.list}
             >
-              <div className={styles.timelineContainer}>
-                <div className={styles.timeline}></div>
-              </div>
               <div className={styles.items}>
                 <div className={styles.emptyItem + " bordered-area"}>-</div>
                 {items.map((item, index) => (
