55b56cfdfc722c22129ec35bee7166592c7b0f09
flaburgan <flaburgan@geexxx.fr>
2024-05-16 23:06:59 +0200

    Do not load locales dynamically, that introduces latency which is not supported by search engines

--- components/App.tsx
+++ components/App.tsx
@@ -1,33 +1,24 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Head from "next/head";
 import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
+import {messages as enMessages} from "../locales/en/messages";
+import {messages as frMessages} from "../locales/fr/messages";
 import { t } from "@lingui/macro";
 import Game from "./game";
 
 export const locales = ["en", "fr"];
 export const defaultLocale = "en";
 
-/**
- * We do a dynamic import of just the catalog that we need
- * @param locale any locale string
- */
-export async function dynamicActivate(locale: string) {
-  const { messages } = await import(`../locales/${locale}/messages`);
-  i18n.load(locale, messages);
-  i18n.activate(locale);
-}
-
 export default function App() {
-  useEffect(() => {
-    const lang = navigator.language.substring(0,2);
-    // With this method we dynamically load the catalogs
-    dynamicActivate(locales.includes(lang) ? lang : defaultLocale);
-  }, []);
+  const lang = navigator.language.substring(0,2);
+  const locale = locales.includes(lang) ? lang : defaultLocale;
+  i18n.load(locale, locale === "en" ? enMessages : frMessages);
+  i18n.activate(locale);
 
   return <I18nProvider i18n={i18n}>
     <Head>
-      <title>disCO2very - {t`Discover the CO2 footprint of actions or objects by ordering them`}</title>
+      <title>disCO2very - {t`Order items to discover their CO2 footprint!`}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1"/>
     </Head>
