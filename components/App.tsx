f837672537415a09659a7c0d050d7c276a3885b5
Fla <flaburgan@geexxx.fr>
2024-05-25 12:11:55 +0200

    Add basic metadata, fixes #31 (#36)

--- components/App.tsx
+++ components/App.tsx
@@ -21,6 +21,14 @@ export default function App() {
       <title>disCO2very - {t`Order items to discover their CO2 footprint!`}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <meta name="description" content="disCO2very is a free game introducing you to the orders of magnitude of the CO2 footprint" />
+      <meta name="theme-color" content="#23272c" />
+      <meta property="og:url" content="https://disco2very.org" />
+      <meta property="og:title" content="disCO2very" />
+      <meta property="og:site_name" content="disCO2very" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content={locale} />
+      <meta property="og:description" content="disCO2very is a free game introducing you to the orders of magnitude of the CO2 footprint" />
     </Head>
     <Game />
   </I18nProvider>
